8bd057dc1cbc8cd27f51e3929a14d89b29538067
pavansachi <pavan8sachi@gmail.com>
2019-09-06 00:07:42 +1000

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,10 +3,3 @@
 touch version.txt
 echo "1.0" > version.txt
 ls -l
-
-git config user.email "dev@github.com"
-git config user.name "Pavan"
-
-git add .
-git status
-git commit -m "checking in version"
